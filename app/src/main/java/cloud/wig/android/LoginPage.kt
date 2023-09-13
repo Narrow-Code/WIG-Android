19a475db58a24d72bdb8fe018b5e995612d657ef
stitchy <matt.mccaughey91@gmail.com>
2023-09-13 14:27:21 -0400

    Add functionality to login button

--- app/src/main/java/cloud/wig/android/LoginPage.kt
+++ app/src/main/java/cloud/wig/android/LoginPage.kt
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint
 import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import cloud.wig.android.databinding.LoginPageBinding
 
 class LoginPage : AppCompatActivity() {
@@ -12,14 +14,24 @@ class LoginPage : AppCompatActivity() {
     @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_page)
+        Log.d("LoginPage", "onCreate method is executing")
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT // Portrait lock
 
-        // Lock layout to portrait
-        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
-
-        // Set bindings
-        binding = LoginPageBinding.inflate(layoutInflater)
+        binding = LoginPageBinding.inflate(layoutInflater) // set bindings to login_page layout id's
         val view = binding.root
-        setContentView(view)
+        setContentView(view) // Open login_page view
+
+        binding.loginPageLoginButton.setOnClickListener {
+            // TODO check username and password
+            val username = binding.loginPageUsername.text.toString()
+            val password = binding.loginPagePassword.text.toString()
+
+            if(username == "stitchy" && password == "Test123" || username == "solo" && password == "Test123"){
+                binding.invalidUserPassword.visibility = View.INVISIBLE
+            } else{
+                binding.invalidUserPassword.visibility = View.VISIBLE
+            }
+
+        }
     }
 }
--- app/src/main/java/cloud/wig/android/MainActivity.kt
+++ app/src/main/java/cloud/wig/android/MainActivity.kt
@@ -23,7 +23,5 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
-        
-        setContentView(R.layout.activity_main)
     }
 }
