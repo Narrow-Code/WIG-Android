6178247209aad4c7dc4935670c0db96edf1ef6d7
stitchy <matt.mccaughey91@gmail.com>
2023-11-27 13:29:49 -0500

    #38 fix PostScanResponse to match server side

--- app/src/main/java/cloud/wig/android/api/items/dto/PostScanResponse.kt
+++ app/src/main/java/cloud/wig/android/api/items/dto/PostScanResponse.kt
@@ -25,5 +25,49 @@ data class Ownership(
     val item_tags: String,
     val item_quantity: Int,
     val item_checked_out: String,
-    val item_borrower: Int
+    val item_borrower: Int,
+    val User: User,
+    val Location: Location,
+    val Item: Item,
+    val Borrower: Borrower
+)
+
+@Serializable
+data class User(
+    val user_uid: Int,
+    val username: String,
+    val email: String,
+    val salt: String,
+    val hash: String,
+    val email_confirm: String,
+    val tier_level: String
+)
+
+@Serializable
+data class Location(
+    val location_uid: Int,
+    val location_owner: Int,
+    val location_name: String,
+    val location_type: String,
+    val location_location: Int,
+    val location_qr: String,
+    val location_tags: String,
+    val location_description: String,
+    val User: User,
+    val Location: Location?
+)
+
+@Serializable
+data class Item(
+    val item_uid: Int,
+    val barcode: String,
+    val item_name: String,
+    val item_brand: String,
+    val item_img: String
+)
+
+@Serializable
+data class Borrower(
+    val borrower_uid: Int,
+    val borrower_name: String
 )
