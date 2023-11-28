119b46c2ecb04c69dfc58823b344318d5a87bbb6
stitchy <matt.mccaughey91@gmail.com>
2023-11-28 18:42:38 -0500

    #41 refactor Scanner

--- app/src/main/java/wig/activities/Scanner.kt
+++ app/src/main/java/wig/activities/Scanner.kt
@@ -30,7 +30,6 @@ private const val CAMERA_REQUEST_CODE = 101
 
 @Suppress("DEPRECATION")
 class Scanner : BaseActivity() {
-    // Set variables
     private lateinit var codeScanner: CodeScanner
     private var pageView = "items"
     private val handler = Handler()
@@ -53,26 +52,122 @@ class Scanner : BaseActivity() {
         scannerBinding.icSettings.setOnClickListener{ logout() }
     }
 
+    private suspend fun scanBarcode(barcode: String) = withContext(Dispatchers.IO){
+        val posts = service.scan(barcode)
+        if (posts.success) {
+            populateItems(posts)
+        } else {
+            // TODO handle what else noise?
+            codeScanner.startPreview()
+        }
+    }
+
+    private fun populateItems(postScanResponse: ScanResponse){
+        val tableLayout = scannerBinding.itemsTableLayout
+        val row = TableRow(this)
+        val layoutParams = TableRow.LayoutParams(
+            TableRow.LayoutParams.MATCH_PARENT,
+            TableRow.LayoutParams.WRAP_CONTENT
+        )
+
+        val nameTextView = TextView(this)
+        nameTextView.text = postScanResponse.ownership[0].item.itemName.substring(0 until 20.coerceAtMost(postScanResponse.ownership[0].item.itemName.length))
+        nameTextView.layoutParams = TableRow.LayoutParams(
+            0,
+            TableRow.LayoutParams.WRAP_CONTENT,
+            1f
+        )
+
+        val locationTextView = TextView(this)
+        locationTextView.text = postScanResponse.ownership[0].location.locationName.substring(0 until 18.coerceAtMost(postScanResponse.ownership[0].location.locationName.length))
+        locationTextView.layoutParams = TableRow.LayoutParams(
+            0,
+            TableRow.LayoutParams.WRAP_CONTENT,
+            1f
+        )
+        locationTextView.gravity = Gravity.CENTER
+
+        val quantityTextView = TextView(this)
+        quantityTextView.text = postScanResponse.ownership[0].itemQuantity.toString()
+        quantityTextView.layoutParams = TableRow.LayoutParams(
+            0,
+            TableRow.LayoutParams.WRAP_CONTENT,
+            1f
+        )
+        quantityTextView.gravity = Gravity.END
+
+        row.addView(nameTextView)
+        row.addView(locationTextView)
+        row.addView(quantityTextView)
+
+        row.layoutParams = layoutParams
+
+        tableLayout.addView(row)
+
+        handler.postDelayed({
+            // Enable scanning after 5 seconds
+            codeScanner.startPreview()
+        }, 1500)
+
+    }
+
+    private fun switchToBinsView() {
+        scannerBinding.tableItemsTitles.visibility = View.INVISIBLE
+        scannerBinding.tableShelvesTitles.visibility = View.INVISIBLE
+        scannerBinding.itemsTable.visibility = View.INVISIBLE
+        scannerBinding.shelvesTable.visibility = View.INVISIBLE
+        scannerBinding.tableBinsTitles.visibility = View.VISIBLE
+        scannerBinding.binsTable.visibility = View.VISIBLE
+        pageView = "bins"
+    }
+
+    private fun switchToItemsView() {
+        scannerBinding.tableBinsTitles.visibility = View.INVISIBLE
+        scannerBinding.tableShelvesTitles.visibility = View.INVISIBLE
+        scannerBinding.binsTable.visibility = View.INVISIBLE
+        scannerBinding.shelvesTable.visibility = View.INVISIBLE
+        scannerBinding.tableItemsTitles.visibility = View.VISIBLE
+        scannerBinding.itemsTable.visibility = View.VISIBLE
+        pageView = "items"
+    }
+
+    private fun switchToShelvesView() {
+        scannerBinding.tableItemsTitles.visibility = View.INVISIBLE
+        scannerBinding.tableBinsTitles.visibility = View.INVISIBLE
+        scannerBinding.binsTable.visibility = View.INVISIBLE
+        scannerBinding.itemsTable.visibility = View.INVISIBLE
+        scannerBinding.tableShelvesTitles.visibility = View.VISIBLE
+        scannerBinding.shelvesTable.visibility = View.VISIBLE
+        pageView = "shelves"
+    }
+
+    // TODO REMOVE THIS IS FOR TESTING LOG IN AND OUT UNTIL SETTINGS PAGE IS ADDED
+    private fun logout() {
+        // Delete token & UID
+        lifecycleScope.launch {
+            val storeToken = StoreToken(this@Scanner)
+            storeToken.saveToken("")
+        }
+        startActivityLogin()
+    }
+
     private fun codeScanner() {
         codeScanner = CodeScanner(this, scannerBinding.scannerView)
 
         codeScanner.apply {
             camera = CodeScanner.CAMERA_BACK
             formats = listOf(BarcodeFormat.UPC_A, BarcodeFormat.UPC_E, BarcodeFormat.EAN_13, BarcodeFormat.EAN_8, BarcodeFormat.QR_CODE)
-
-
             autoFocusMode = AutoFocusMode.SAFE
             scanMode = ScanMode.SINGLE
             isAutoFocusEnabled = true
             isFlashEnabled = false
-
             decodeCallback = DecodeCallback {
                 runOnUiThread {
-                    // newText.text = it.text
-
+                    codeScanner.stopPreview()
                     if(it.barcodeFormat != BarcodeFormat.QR_CODE){
-                        scanBarcodeAPICall(it.text)
-                        codeScanner.startPreview()
+                        lifecycleScope.launch {
+                        scanBarcode(it.text)
+                        }
                     }
                 }
             }
@@ -81,7 +176,6 @@ class Scanner : BaseActivity() {
                 }
             }
         }
-
         scannerBinding.scannerView.setOnClickListener {
             codeScanner.startPreview()
         }
@@ -99,19 +193,13 @@ class Scanner : BaseActivity() {
 
     private fun setupPermissions() {
         val permission : Int = ContextCompat.checkSelfPermission(this,
-            Manifest.permission.CAMERA
-        )
-
+            Manifest.permission.CAMERA)
         if (permission != PackageManager.PERMISSION_GRANTED) {
-            makeRequest()
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.CAMERA),
+                CAMERA_REQUEST_CODE
+            )
         }
-    }
-
-    private fun makeRequest() {
-        ActivityCompat.requestPermissions(this,
-            arrayOf(Manifest.permission.CAMERA),
-            CAMERA_REQUEST_CODE
-        )
     }
 
     @SuppressLint("MissingSuperCall")
@@ -123,123 +211,9 @@ class Scanner : BaseActivity() {
             CAMERA_REQUEST_CODE -> {
                 if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED){
                     Toast.makeText(this, "You need the camera permission to be able to use this app!", Toast.LENGTH_SHORT).show()
-                } else {
-                    //successful
                 }
             }
         }
     }
-
-    //TODO REMOVE THIS IS FOR TESTING LOG IN AND OUT UNTIL SETTINGS PAGE IS ADDED
-    private fun logout() {
-        // Delete token & UID
-        lifecycleScope.launch {
-            val storeToken = StoreToken(this@Scanner)
-            storeToken.saveToken("")
-        }
-        startActivityLogin()
-    }
-
-    private fun switchToBinsView() {
-        scannerBinding.tableItemsTitles.visibility = View.INVISIBLE
-        scannerBinding.tableShelvesTitles.visibility = View.INVISIBLE
-        scannerBinding.itemsTable.visibility = View.INVISIBLE
-        scannerBinding.shelvesTable.visibility = View.INVISIBLE
-        scannerBinding.tableBinsTitles.visibility = View.VISIBLE
-        scannerBinding.binsTable.visibility = View.VISIBLE
-
-        pageView = "bins"
-    }
-
-    private fun switchToItemsView() {
-        scannerBinding.tableBinsTitles.visibility = View.INVISIBLE
-        scannerBinding.tableShelvesTitles.visibility = View.INVISIBLE
-        scannerBinding.binsTable.visibility = View.INVISIBLE
-        scannerBinding.shelvesTable.visibility = View.INVISIBLE
-        scannerBinding.tableItemsTitles.visibility = View.VISIBLE
-        scannerBinding.itemsTable.visibility = View.VISIBLE
-
-        pageView = "items"
-    }
-
-    private fun switchToShelvesView() {
-        scannerBinding.tableItemsTitles.visibility = View.INVISIBLE
-        scannerBinding.tableBinsTitles.visibility = View.INVISIBLE
-        scannerBinding.binsTable.visibility = View.INVISIBLE
-        scannerBinding.itemsTable.visibility = View.INVISIBLE
-        scannerBinding.tableShelvesTitles.visibility = View.VISIBLE
-        scannerBinding.shelvesTable.visibility = View.VISIBLE
-
-        pageView = "shelves"
-    }
-
-    private fun populateItems(postScanResponse: ScanResponse){
-        val tableLayout = scannerBinding.itemsTableLayout
-        val row = TableRow(this@Scanner)
-
-        val layoutParams = TableRow.LayoutParams(
-            TableRow.LayoutParams.MATCH_PARENT,
-            TableRow.LayoutParams.WRAP_CONTENT
-        )
-
-        val nameTextView = TextView(this@Scanner)
-        nameTextView.text = postScanResponse.ownership[0].item.itemName.substring(0 until 20.coerceAtMost(postScanResponse.ownership[0].item.itemName.length))
-        nameTextView.layoutParams = TableRow.LayoutParams(
-            0,
-            TableRow.LayoutParams.WRAP_CONTENT,
-            1f
-        )
-
-        val locationTextView = TextView(this@Scanner)
-        locationTextView.text = postScanResponse.ownership[0].location.locationName.substring(0 until 18.coerceAtMost(postScanResponse.ownership[0].location.locationName.length))
-        locationTextView.layoutParams = TableRow.LayoutParams(
-            0,
-            TableRow.LayoutParams.WRAP_CONTENT,
-            1f
-        )
-        locationTextView.gravity = Gravity.CENTER
-
-        val quantityTextView = TextView(this@Scanner)
-        quantityTextView.text = postScanResponse.ownership[0].itemQuantity.toString()
-        quantityTextView.layoutParams = TableRow.LayoutParams(
-            0,
-            TableRow.LayoutParams.WRAP_CONTENT,
-            1f
-        )
-        quantityTextView.gravity = Gravity.END
-
-        row.addView(nameTextView)
-        row.addView(locationTextView)
-        row.addView(quantityTextView)
-
-        row.layoutParams = layoutParams
-
-        tableLayout.addView(row)
-
-        codeScanner.stopPreview()
-
-        handler.postDelayed({
-            // Enable scanning after 5 seconds
-            codeScanner.startPreview()
-        }, 1500)
-
-    }
-
-    private fun scanBarcodeAPICall(barcode: String) {
-        lifecycleScope.launch {
-            try {
-                val posts = withContext(Dispatchers.IO) {
-                    service.scan(barcode)
-                }
-                if (posts.success) {
-                    populateItems(posts)
-                } else {
-                    // TODO handle what else
-                }
-            } catch (e: Exception) {
-                // TODO handle exception, maybe network issue popup?
-            }
-        }
-    }
-
 }
+
