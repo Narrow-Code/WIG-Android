c03495aae9af40e53b9d5eedce370702b4c67b20
stitchy <matt.mccaughey91@gmail.com>
2023-11-29 08:21:53 -0500

    remove local routes

--- app/src/main/java/wig/api/HttpRoutes.kt
+++ app/src/main/java/wig/api/HttpRoutes.kt
@@ -5,8 +5,7 @@ package wig.api
  */
 object HttpRoutes {
 
-    //private const val BASE_URL = "http://34.234.73.177:30001" // SERVER
-    private const val BASE_URL = "http://192.168.0.201:30001" // LOCAL
+    private const val BASE_URL = "http://34.234.73.177:30001" // SERVER
 
     const val SIGNUP = "$BASE_URL/user/signup"
     const val SALT = "$BASE_URL/user/salt"
