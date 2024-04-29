7ca612b2e185c26a160c54d0fd69d11f314a890e
stitchy <matt.mccaughey91@gmail.com>
2024-04-29 11:36:25 -0400

    add comments to Bindings

--- app/src/main/java/wig/activities/base/Bindings.kt
+++ app/src/main/java/wig/activities/base/Bindings.kt
@@ -11,6 +11,7 @@ import wig.databinding.ServerSetupBinding
 import wig.databinding.SettingsBinding
 import wig.databinding.SignupBinding
 
+// Bindings sets up all of the layout bindings
 open class Bindings : API() {
     protected lateinit var emailVerificationBinding: EmailVerificationBinding
     protected lateinit var forgotPasswordBinding: ForgotPasswordBinding
@@ -23,60 +24,70 @@ open class Bindings : API() {
     protected lateinit var checkedOutBinding: CheckedOutBinding
     protected lateinit var inventoryBinding: InventoryBinding
 
+    // setLoginBindings sets up the Login layout bindings
     protected fun setLoginBindings(){
         loginBinding = LoginBinding.inflate(layoutInflater)
         val view = loginBinding.root
         setContentView(view)
     }
 
+    // setSignupBindings sets up the Signup layout bindings
     protected fun setSignupBindings(){
         signupBinding = SignupBinding.inflate(layoutInflater)
         val view = signupBinding.root
         setContentView(view)
     }
 
+    // setSettingsBindings sets up the Settings layout bindings
     protected fun setSettingsBindings(){
         settingsBinding = SettingsBinding.inflate(layoutInflater)
         val view = settingsBinding.root
         setContentView(view)
     }
 
+    // setForgotPasswordBindings sets up the ForgotPassword layout bindings
     protected fun setForgotPasswordBindings(){
         forgotPasswordBinding = ForgotPasswordBinding.inflate(layoutInflater)
         val view = forgotPasswordBinding.root
         setContentView(view)
     }
 
+    // setCheckedOutBindings sets up the CheckedOut layout bindings
     protected fun setCheckedOutBindings(){
         checkedOutBinding = CheckedOutBinding.inflate(layoutInflater)
         val view = checkedOutBinding.root
         setContentView(view)
     }
 
+    // setInventoryBindings sets up the Inventory layout bindings
     protected fun setInventoryBindings(){
         inventoryBinding = InventoryBinding.inflate(layoutInflater)
         val view = inventoryBinding.root
         setContentView(view)
     }
 
+    // setScannerBindings sets up the Scanner layout bindings
     protected fun setScannerBindings(){
         scannerBinding = ScannerBinding.inflate(layoutInflater)
         val view = scannerBinding.root
         setContentView(view)
     }
 
+    // setResetPasswordBindings sets up the ResetPassword layout bindings
     protected fun setResetPasswordBindings(){
         resetPasswordBinding = ResetPasswordBinding.inflate(layoutInflater)
         val view = resetPasswordBinding.root
         setContentView(view)
     }
 
+    // setEmailVerificationBindings sets up the EmailVerification layout bindings
     protected fun setEmailVerificationBindings(){
         emailVerificationBinding = EmailVerificationBinding.inflate(layoutInflater)
         val view = emailVerificationBinding.root
         setContentView(view)
     }
 
+    // setServerSetupBindings sets up the ServerSetup layout bindings
     protected fun setServerSetupBindings() {
         serverSetupBinding = ServerSetupBinding.inflate(layoutInflater)
         val view = serverSetupBinding.root
