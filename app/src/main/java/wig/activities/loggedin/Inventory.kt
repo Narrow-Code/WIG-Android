89559bbb13b2c61ac2187229fcf1db13fcf19170
stitchy <matt.mccaughey91@gmail.com>
2024-07-24 09:56:32 -0400

    add deleteOwnership

--- app/src/main/java/wig/activities/loggedin/Inventory.kt
+++ app/src/main/java/wig/activities/loggedin/Inventory.kt
@@ -7,6 +7,7 @@ import kotlinx.coroutines.launch
 import wig.activities.base.Settings
 import wig.managers.InventoryExpandableListAdapter
 import wig.models.responses.InventoryDTO
+import wig.utils.Alerts
 
 class Inventory : Settings() {
 
@@ -38,7 +39,36 @@ class Inventory : Settings() {
 
                 adapter = InventoryExpandableListAdapter(this@Inventory, inventory)
                 expandableListView.setAdapter(adapter)
+
+                setOnItemLongClickListener()
             }
+        }
+    }
+
+    private fun setOnItemLongClickListener() {
+        expandableListView.setOnItemLongClickListener { _, _, position, _ ->
+            val packedPosition = expandableListView.getExpandableListPosition(position)
+            val itemType = ExpandableListView.getPackedPositionType(packedPosition)
+            val groupPosition = ExpandableListView.getPackedPositionGroup(packedPosition)
+            if (itemType == ExpandableListView.PACKED_POSITION_TYPE_GROUP) {
+                val ownership = adapter.getGroup(groupPosition)
+                // TODO handle long click on Location
+            } else {
+                val childPosition = ExpandableListView.getPackedPositionChild(packedPosition)
+                val ownership = adapter.getChild(groupPosition, childPosition)
+                Alerts().deleteConfirmation(ownership.customItemName, this) { shouldDelete ->
+                    if (shouldDelete) {
+                        lifecycleScope.launch {
+                            val result = api.deleteOwnership(ownership.ownershipUID)
+                            if(result.success){
+                                adapter.removeChild(groupPosition, childPosition)
+                            }
+                        }
+                    }
+                }
+
+            }
+            true
         }
     }
 
